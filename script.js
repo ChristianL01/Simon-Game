6227092d4b2254742e0f1ab301c7e2206a290bb2
ChristianL01 <locke.christian1@gmail.com>
2022-09-12 07:29:48 -0500

    choose tile

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-let sequence = [];
+let level = 0;
+let cpuSequence = [];
 let humanSequence = [];
+
+
 const startButton = document.querySelector('.start');
+const redTile = document.querySelector('.color-red');
+const blueTile = document.querySelector('.color-blue');
+const yellowTile = document.querySelector('.color-yellow');
+const greenTile = document.querySelector('.color-yellow');
+
 
 function startGame() {
-    
+    startButton.classList.add('remove');
+    window.alert('Wait for the sequence then repeat it!')
 }
+startButton.addEventListener('click', startGame)
+
+function currentSequence() {
+    const colors = ['red', 'yellow', 'blue', 'green'];
+    const random = colors[Math.floor(Math.random() * colors.length)];
+
+    return random
+}
+console.log(currentSequence())
